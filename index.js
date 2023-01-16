b5fa6c6f29e45a3c1d224406e9e8ee84d5ac9d38
rismanharism <risman.haris6@gmail.com>
2023-01-16 20:47:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Insert your code here
+var massa = 10;
+var percepatan = 9;
+let hasil = massa * percepatan
+console.log(hasil);
+/*
+algoritma
+memasukan data sebuah mobil memiliki 600 kg
+lalu mobil tersebut didorong dengan 3 orang percepatannya 2 m/s2
+hasil resultan gaya adalah massa mobil 600 kg di kali percepatan benda yang di beri gaya 2 m/s2
+maka hasil 1.200 m/s2
+
+pseudocode
+STORE beratMobil AS NUMBER WTIH 600 
+STORE percepatanMobil AS NUMBER 2 
+STORE resultanGaya AS CALCULATED beratMobil TIMES percepatanMobil
+SET resultanGaya VALUE WITH CALCULATION RESULT
+DISPLAY resultanGaya
+**/
